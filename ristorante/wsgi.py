2c64cc5d3751fb5568d2ab51587791d55a4e0552
Lorenzo Dinardo <111635352+LorenzoDinardo00@users.noreply.github.com>
2024-10-17 01:41:04 +0200

    Update wsgi.py

--- ristorante/wsgi.py
+++ ristorante/wsgi.py
@@ -14,3 +14,8 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ristorante.settings')
 
 application = get_wsgi_application()
+# Esegui le migrazioni all'avvio del server
+try:
+    call_command('migrate')
+except Exception as e:
+    print(f"Errore durante la migrazione: {e}")
