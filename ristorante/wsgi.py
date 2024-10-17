f6eb4ae2ea4395535868f93ab66c60e9c724c016
Lorenzo Dinardo <111635352+LorenzoDinardo00@users.noreply.github.com>
2024-10-17 02:11:37 +0200

    Update wsgi.py

--- ristorante/wsgi.py
+++ ristorante/wsgi.py
@@ -6,10 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
-
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ristorante.settings')
 
