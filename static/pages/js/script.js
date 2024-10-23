156e642d2905c4019a7d81e67e9929ec18a967e6
lorenzo <lorenzo.dinardo@stud.unifi.it>
2024-10-23 15:18:48 +0200

    TestVelocità

--- static/pages/js/script.js
+++ static/pages/js/script.js
@@ -1,20 +1,3 @@
-// slider.js
-let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("slider-image");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 5000); // Cambia immagine ogni 5 secondi
-}
-
-
 document.addEventListener('DOMContentLoaded', function () {
     const navbarHeight = document.querySelector('.navbar').offsetHeight;
 
